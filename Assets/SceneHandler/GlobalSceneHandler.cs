ca86f1f56245e4ff263699610aa30030b1c59cc9
Eddio0141 <eddio0141@gmail.com>
2023-02-03 18:42:23 +0000

    fixed base of scene handler

---
+++ Assets/SceneHandler/GlobalSceneHandler.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using Scenes.SceneTypes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SceneHandler
+{
+    public static class GlobalSceneHandler
+    {
+        /// <summary>
+        /// List of all loaded scenes
+        /// </summary>
+        private static readonly List<SceneBase> LoadedScenes = new();
+
+        public static AsyncOperation LoadScene(Scene scene, bool overwrite = true)
+        {
+            var sceneInstance = NewScene(scene);
+
+            if (overwrite)
+            {
+                LoadedScenes.Clear();
+                LoadedScenes.Add(sceneInstance);
+                return SceneManager.LoadSceneAsync(sceneInstance.SceneName);
+            }
+
+            LoadedScenes.Add(sceneInstance);
+
+            return SceneManager.LoadSceneAsync(sceneInstance.SceneName, LoadSceneMode.Additive);
+        }
+
+        public static void UnloadScene(Scene scene)
+        {
+            var sceneInstance = NewScene(scene);
+
+            LoadedScenes.RemoveAll(x => x.SceneName == sceneInstance.SceneName);
+
+            SceneManager.UnloadSceneAsync(sceneInstance.SceneName);
+        }
+
+        public static void PauseScene(Scene scene)
+        {
+            var sceneInstance = NewScene(scene);
+            sceneInstance.Pause();
+        }
+
+        public static void TempUnloadScene(Scene scene)
+        {
+            var sceneInstance = NewScene(scene);
+            sceneInstance.TempUnload();
+        }
+
+        private static SceneBase NewScene(Scene scene)
+        {
+            var sceneType = SceneType(scene);
+
+            return (SceneBase)Activator.CreateInstance(sceneType);
+        }
+
+        private static Type SceneType(Scene scene)
+        {
+            return scene switch
+            {
+                Scene.TestWorld => typeof(TestWorldScene),
+                Scene.EnemyBattle => typeof(EnemyBattleScene),
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+
+    public enum Scene
+    {
+        TestWorld,
+        EnemyBattle
+    }
+}
--- docs/scene-transision.md
+++ docs/scene-transision.md
@@ -1,11 +1,14 @@
 # Purpose of having a scene handler
-Rather than using unity's scene changing directly, I created a class `SceneHandler` which handles all scene changing stuff
+Rather than using unity's scene changing directly, I created a class `GlobalSceneHandler` which handles all scene changing stuff
 
 # Functionality
-## Background scenes
-- Functions that takes the `Background` enum will automatically load that background as an additional scene
+## Loading a scene
+- The enum `Scene` will automatically choose which scene to load
+- You can load scenes as additional scenes, or overwrite loaded scenes
 
-## Battle scene
-- You can transition to all of the game's battle scenes, and give this function the transition effect coroutine in order to choose your own transition effect
-- Because how after the battle ends, we must resume the normal gameplay from where it got cut off, I need to have a way that doesn't unload the main scene, but keeps it "inactive in the background"
-  - This I can solve with a base "world" game object class called `WorldObject`, which has extra functionality to help the main scene objects to pause while in battle scene
+## Pausing a scene
+- If you have a loaded scene, you can "pause" everything in it
+- There's a resume function too
+
+## Temporary unload a scene
+- You can unload a scene so it's invisible
