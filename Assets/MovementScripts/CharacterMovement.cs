ebf0a51d6869d922ae656d38754eee2bcd766dd6
Eddio0141 <eddio0141@gmail.com>
2023-02-03 17:30:14 +0000

    added scene transition handler

--- Assets/MovementScripts/CharacterMovement.cs
+++ Assets/MovementScripts/CharacterMovement.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour
+public class CharacterMovement : WorldObject
 {
     private Vector2 _inputDirection;
     private Rigidbody2D _rigidbody;
     [SerializeField] private float speed = 5f;
+
+    private bool _battlePause;
 
     private void Start()
     {
@@ -24,16 +26,32 @@ public class CharacterMovement : MonoBehaviour
     private void Move()
     {
         // Move the player
-        _rigidbody.MovePosition(_rigidbody.position + _inputDirection * speed * Time.fixedDeltaTime);
+        _rigidbody.MovePosition(_rigidbody.position + _inputDirection * (speed * Time.fixedDeltaTime));
     }
 
     private void Update()
     {
+        if (_battlePause)
+            return;
         ProcessInput();
     }
 
     private void FixedUpdate()
     {
+        if (_battlePause)
+            return;
         Move();
+    }
+
+    public override void BattleStart()
+    {
+        base.BattleStart();
+        _battlePause = true;
+    }
+
+    public override void BattleEnd()
+    {
+        base.BattleEnd();
+        _battlePause = false;
     }
 }
--- Assets/SceneHandler.cs
+++ Assets/SceneHandler.cs
@@ -1,12 +1,25 @@
-using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneHandler : MonoBehaviour
 {
     private static SceneHandler _instance;
-    
-    private Coroutine _currentTransition;
+
+    private Coroutine _currentTransitionWait;
+    private ITransitionEffect _currentTransition;
+
+    private AsyncOperation _nextScene;
+    private AsyncOperation _backgroundScene;
+
+    private const string EnemyBattleScene = "EnemyBattle";
+
+    private readonly string[] _bgScenes = { "BGTest" };
+    private string _loadedBgScene;
+
+    private WorldObject[] _worldObjectsCache;
+
+    // TODO create a way to load additional scenes and unload them, unique class for each way?
 
     private void Start()
     {
@@ -15,26 +28,121 @@ public class SceneHandler : MonoBehaviour
             Destroy(this);
             return;
         }
-        
+
         // safety check to make sure there is only one instance of this class
         _instance = this;
         DontDestroyOnLoad(this);
     }
 
-    public void EnemyBattleStart(Background background, IEnemyBattleTransition transition)
+    public void EnemyBattleStart(Background background, ITransitionEffect transition)
     {
-        if (_currentTransition != null)
-            StopCoroutine(_currentTransition);
-        _currentTransition = StartCoroutine(transition.TransitionEffect());
+        _worldObjectsCache = FindObjectsOfType<WorldObject>();
+        foreach (var worldObject in _worldObjectsCache)
+        {
+            worldObject.BattleStart();
+        }
+
+        if (_currentTransitionWait != null)
+            StopCoroutine(_currentTransitionWait);
+        _currentTransition = transition;
+        StartCoroutine(BattleTransitionStartWait(background));
     }
 
-    public enum Background
+    public void EnemyBattleEnd(ITransitionEffect transition)
     {
-        Test
+        foreach (var worldObject in _worldObjectsCache)
+        {
+            worldObject.BattleEnd();
+        }
+
+        if (_currentTransitionWait != null)
+            StopCoroutine(_currentTransitionWait);
+        _currentTransition = transition;
+        StartCoroutine(BattleTransitionEndWait());
+    }
+
+    private IEnumerator BattleTransitionStartWait(Background? background)
+    {
+        _nextScene = SceneManager.LoadSceneAsync(EnemyBattleScene, LoadSceneMode.Additive);
+        _nextScene.allowSceneActivation = false;
+        _backgroundScene = BackgroundSceneLoad(background);
+
+        StartCoroutine(_currentTransition.TransitionEffect());
+
+        // wait for transition to finish
+        yield return new WaitUntil(() => _currentTransition.IsDone);
+        Debug.Log("Finished transition, loading battle scene");
+        foreach (var worldObject in _worldObjectsCache)
+        {
+            worldObject.FinishedStartTransition();
+        }
+
+        _currentTransitionWait = null;
+        _nextScene.allowSceneActivation = true;
+        _nextScene = null;
+        if (_backgroundScene != null)
+        {
+            _backgroundScene.allowSceneActivation = true;
+        }
+    }
+
+    private IEnumerator BattleTransitionEndWait()
+    {
+        SceneManager.UnloadSceneAsync(EnemyBattleScene);
+        BackgroundSceneUnload();
+
+        StartCoroutine(_currentTransition.TransitionEffect());
+
+        // wait for transition to finish
+        yield return new WaitUntil(() => _currentTransition.IsDone);
+        foreach (var worldObject in _worldObjectsCache)
+        {
+            worldObject.FinishedEndTransition();
+        }
+
+        _currentTransitionWait = null;
+        _nextScene.allowSceneActivation = true;
+        _nextScene = null;
+    }
+
+    private AsyncOperation BackgroundSceneLoad(Background? background)
+    {
+        if (background == null) return null;
+
+        if (_loadedBgScene != null)
+        {
+            SceneManager.UnloadSceneAsync(_loadedBgScene);
+        }
+
+        if ((int)background.Value > _bgScenes.Length)
+        {
+            Debug.LogError("Background scene index out of range");
+            return null;
+        }
+
+        _loadedBgScene = _bgScenes[(int)background.Value];
+        var operation = SceneManager.LoadSceneAsync(_loadedBgScene, LoadSceneMode.Additive);
+        operation.allowSceneActivation = false;
+        return operation;
+    }
+
+    private void BackgroundSceneUnload()
+    {
+        if (_loadedBgScene != null)
+        {
+            SceneManager.UnloadSceneAsync(_loadedBgScene);
+            _loadedBgScene = null;
+        }
     }
 }
 
-public interface IEnemyBattleTransition
+public enum Background
+{
+    Test
+}
+
+public interface ITransitionEffect
 {
     IEnumerator TransitionEffect();
+    bool IsDone { get; }
 }
