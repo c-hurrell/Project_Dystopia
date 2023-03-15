68c1fd41b95103afb0dbe605e532e63fceec47a1
Charania1 <25154050@students.lincoln.ac.uk>
2023-03-15 11:57:30 +0000

    part drop sub made and item drop commented for further review

--- Assets/Stat_Classes/Enemy_Character.cs
+++ Assets/Stat_Classes/Enemy_Character.cs
@@ -7,11 +7,22 @@ using Stat_Classes;
 
     public class Enemy_Character : Character    //maybe should be a virtual class for specific enemy types to inherit from
     {
-        List<item> dropList;
+    /*    List<item> dropList;
         item selectdrop()
         {
             int selector = Random.Range(0,dropList.length);
             return dropList[selector];
+
+        }*/
+        void onDeath(){
+          List<Part> partDrops = new List<Part>();
+          partDrops.Add(headPart);
+          partDrops.Add(chestPart);
+          partDrops.Add(armsPart);
+          partDrops.Add(legsPart);
+          partDrops.Add(personaCore);
+          partDrops.Add(powerCore);
+         
 
         }
     }
