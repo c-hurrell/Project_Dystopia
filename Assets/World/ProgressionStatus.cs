05da0795f08936fb042512f4c9e97a52bd1d4381
Eddio0141 <eddio0141@gmail.com>
2023-03-15 14:38:49 +0000

    added way to track current area in world

---
+++ Assets/World/ProgressionStatus.cs
@@ -0,0 +1,12 @@
+namespace World
+{
+    public static class ProgressionStatus
+    {
+        public static Area CurrentArea { get; } = Area.Default;
+    }
+
+    public enum Area
+    {
+        Default
+    }
+}
