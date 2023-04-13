6686cbaeeaab73470e60553d14d7aec6a7a069be
Eddio0141 <eddio0141@gmail.com>
2023-04-13 17:08:36 +0100

    added death

--- Assets/Combat/EnemyBattleStatus.cs
+++ Assets/Combat/EnemyBattleStatus.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Combat
@@ -7,5 +8,23 @@ namespace Combat
         public int health;
         public int attack;
         public int speed;
+
+        public void TakeDamage(int damage)
+        {
+            health -= damage;
+            Debug.Log("Enemy took damage " + damage + " and health is now " + health);
+
+            if (health <= 0)
+            {
+                Debug.Log("Enemy died");
+                StartCoroutine(Die());
+            }
+        }
+
+        private IEnumerator Die()
+        {
+            yield return new WaitForSeconds(2.0f);
+            Destroy(gameObject);
+        }
     }
 }
