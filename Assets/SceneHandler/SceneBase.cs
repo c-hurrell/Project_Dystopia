0e8409f486c385f6036240acafac3a36f5f265d4
Eddio0141 <eddio0141@gmail.com>
2023-04-14 16:06:23 +0100

    option to additively load scenes

--- Assets/SceneHandler/SceneBase.cs
+++ Assets/SceneHandler/SceneBase.cs
@@ -10,6 +10,11 @@ namespace SceneHandler
         public abstract string SceneName { get; }
 
         /// <summary>
+        /// If the scene should be loaded additively
+        /// </summary>
+        public virtual bool Additive => false;
+
+        /// <summary>
         /// Coroutine called when the scene is about to load
         /// </summary>
         public virtual IEnumerator Load()
