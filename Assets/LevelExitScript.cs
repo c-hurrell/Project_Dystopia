7dcf1a24105691a21b1fe5ace83596e494e0a360
c-hurrell <charliehurrell2805@gmail.com>
2023-05-10 01:22:09 +0100

    Set up actual parts for gameplay

--- Assets/LevelExitScript.cs
+++ Assets/LevelExitScript.cs
@@ -7,8 +7,12 @@ public class LevelExitScript : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private string nextLevel = "Level2";
-    private void OnCollisionEnter()
+    [SerializeField] private BoxCollider2D exitBox;
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        if (!other.CompareTag("Player")) return;
+        
+        Debug.Log("Player entered battle trigger");
         SceneManager.LoadScene(nextLevel);
     }
 }
